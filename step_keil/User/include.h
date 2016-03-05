7b95be024e373804e9502b002a13496c704c7164
huipeng <huipengly@gmail.com>
2016-03-05 11:41:49 +0800

    初始化

---
+++ step_keil/User/include.h
@@ -0,0 +1,25 @@
+/*************************************************
+  Copyright (C), 2015 - 2015, ZhA.
+  FileName: include.h
+  Author: NaTel  Version: 0.X     Date: 2015/08/04 
+  Description: 工程模版-此文件包含了所有用到的头文件
+  Others: 具体说明见各自头文件
+  Function List: 
+	例如：
+	1. Init.h               //初始化设置及函数头文件
+  History:        // 修改历史记录列表，每条修改记录应包括修改日期、修改
+                  // 者及修改内容简述
+    1. Date:
+       Author:
+       Modification:
+ 
+    2. ...
+*************************************************/
+#ifndef __INCLUDE_H
+#define __INCLUDE_H
+
+//例如#include "Init.h"
+
+#endif /*__INCLUDE_H*/
+
+/**************END OF FILE****************/
